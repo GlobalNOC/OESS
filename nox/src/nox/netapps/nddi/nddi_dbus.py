8f8433d2d411d20d50a333c8dcad2e5277c1c661
AJ Ragusa <aragusa@grnoc.iu.edu>
2012-08-01 17:21:28 +0000

    ISSUE=4119 adding the ability for nox to send a drop all rule with low priority

--- nox/src/nox/netapps/nddi/nddi_dbus.py
+++ nox/src/nox/netapps/nddi/nddi_dbus.py
@@ -141,6 +141,18 @@ class dBusEventGen(dbus.service.Object):
                          in_signature='t',
                          out_signature='t'
                          )
+
+    def install_default_drop(self, dpid):
+        my_attrs          = {}
+        actions           = []
+        
+        idle_timeout = 0
+        hard_timeout = 0
+        
+        _do_install(dpid, lambda: inst.install_datapath_flow(dp_id=dpid, attrs=my_atrrs, idle_timeout=idle_timout, hard_timeout=hard_timeout, actions=actions,priority=0x0001 inport=None));
+
+        return dpid
+                   
     def install_default_forward(self, dpid):
         my_attrs          = {}
         my_attrs[DL_TYPE] = 0x88cc       
