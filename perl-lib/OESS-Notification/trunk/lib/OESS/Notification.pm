b1f7ac93e3c5a519e3e9d26f08b015cb39a6c7a0
Grant McNaught <gmcnaugh@grnoc.iu.edu>
2013-05-04 19:30:26 +0000

    ISSUE=5424 migrated 'syslogger' functionality to OESS::Notification

--- perl-lib/OESS-Notification/trunk/lib/OESS/Notification.pm
+++ perl-lib/OESS-Notification/trunk/lib/OESS/Notification.pm
@@ -10,6 +10,12 @@ use GRNOC::WebService::Client;
 use Data::Dumper;
 use MIME::Lite;
 use Template;
+
+use Net::DBus::Exporter qw (org.nddi.notification);
+use Net::DBus qw(:typing);
+
+use base qw(Net::DBus::Object);
+
 #--------------------------------------------------------------------
 
 #----- OESS::Notification
@@ -55,9 +61,13 @@ our $VERSION = '1.0.0';
 sub new {
     my $that  = shift;
     my $class = ref($that) || $that;
+    #my $service = shift;
     my %args  = @_;
-    my $self  = \%args;
-
+    my $service = $args{'service'};
+    #my $self  = \%args;
+    
+    my $self = $class->SUPER::new($args{'service'}, "/controller1" );
+    $self->{'config_file'} = $args{'config_file'};
     return if !defined( $self->{'config_file'} );
 
     bless $self, $class;
@@ -65,9 +75,94 @@ sub new {
     $self->_process_config_file();
     $self->_connect_services();
     $self->_build_templates();
+
+    dbus_signal("signal_circuit_provision", [["dict","string",["variant"]]],['string']);
+	dbus_signal("signal_circuit_modify", [["dict","string",["variant"]]]);
+	dbus_signal("signal_circuit_decommission",  [["dict","string",["variant"]]],['string']);
+
+	dbus_method("circuit_provision", [["dict","string",["variant"]]],['string']);
+	dbus_method("circuit_modify", [["dict","string",["variant"]]],['string']);
+	dbus_method("circuit_decommission",  [["dict","string",["variant"]]],['string']);
+    
+
+
+    
     
     return $self;
 }
+
+sub circuit_provision {
+	my $self = shift;
+	my $circuit = shift;
+
+
+    my $circuit_notification_data = $self->get_notification_data(  circuit =>$circuit );   
+    $circuit->{'clr'} = $circuit_notification_data->{'clr'};
+    foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
+
+        $self->send_notification( 
+                                           notification_type =>'provision',
+                                           username => $circuit_notification_data->{'username'},
+                                           contact_data => $user,
+                                           circuit_data => $circuit
+                                           
+                                          );
+    }
+
+
+	$self->emit_signal("signal_circuit_provision", $circuit);
+
+}
+
+
+sub circuit_modify {
+	my $self = shift;
+	my $circuit = shift;
+
+	warn ("in circuit modify");
+
+     my $circuit_notification_data = $self->get_notification_data( circuit => $circuit );   
+    $circuit->{'clr'} = $circuit_notification_data->{'clr'};
+    foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
+        $self->send_notification( 
+                                           notification_type =>'modify',
+                                           username => $circuit_notification_data->{'username'},
+                                           contact_data => $user,
+                                           circuit_data => $circuit
+                                          
+                                          );
+    }
+
+	$self->emit_signal("signal_circuit_modify", $circuit);
+
+}
+
+    
+
+sub circuit_decommission {
+	my $self = shift;
+	my $circuit = shift;
+
+    my $circuit_notification_data = $self->get_notification_data(  circuit =>$circuit );   
+
+    foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
+    $circuit->{'clr'} = $circuit_notification_data->{'clr'};
+        $self->send_notification( 
+                                           notification_type =>'decommission',
+                                           username => $circuit_notification_data->{'username'},
+                                           contact_data => $user,
+                                           circuit_data => $circuit
+                                           
+                                          );
+    }
+
+
+    #in case anything needs to listen to this event
+	$self->emit_signal("signal_circuit_decommission", $circuit);
+
+}
+
+
 
 sub send_notification {
     my $self= shift;
@@ -80,17 +175,20 @@ sub send_notification {
                );
     
     
-    #warn Dumper (\%args);
-    my $subject_map = {
-                        'failover' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Failover",
-                        'modify' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Modified",
-                        'provision' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Provisioned",
-                        'decommission' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Decommissioned"
-                       };
-
+    
+    my $subject_map = {                       
+                       'modify' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Modified",
+                       'provision' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Provisioned",
+                       'decommission' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Decommissioned",
+                       'failover_failed_unknown' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." is down",
+                       'failover_failed_no_backup' =>"OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." is down",
+                       'failover_failed_path_down' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." is down",
+                       'failover_success' => "OESS Notification: Circuit ".$args{'circuit_data'}{'description'}." Successful Failover to alternate path",
+                      };
+    
     my $vars = {
                 circuit_description => $args{'circuit_data'}{'description'},
-                circuit_clr => $args{'circuit_data'}{'clr'},
+                clr => $args{'circuit_data'}{'clr'},
                 from_signature_name => $self->{'from_name'},
                 given_name => $args{'contact_data'}{'first_name'},
                 last_name => $args{'contact_data'}{'family_name'}
@@ -119,7 +217,7 @@ sub _build_templates {
 
 Greetings [%given_name%] [%last_name%],
 
-I'm writing to notify you that the following circuit has been provisioned: 
+The following circuit has been provisioned: 
 
 [%circuit_clr%]
 
@@ -129,11 +227,13 @@ Sincerely,
 
 TEMPLATE
 
-      $self->{'decommission_template'} = <<TEMPLATE;
+$self->{'decommission_template'} = <<TEMPLATE;
 
 Greetings [%given_name%] [%last_name%],
 
-The circuit [%circuit_description%] has been decommissioned.
+The circuit [%circuit_description%] has been decommissioned. For reference, its layout record is below:
+
+[%circuit_clr%]
 
 Sincerely,
 
@@ -158,11 +258,11 @@ Sincerely,
 
 TEMPLATE
 
-$self->{'failover_template'} = <<TEMPLATE;
+$self->{'failover_success_template'} = <<TEMPLATE;
 
 Greetings [%given_name%] [%last_name%],
 
-The following circuit has been failed over:
+The following circuit has successfully failed over to an alternate path:
 
 The details of the circuit as they are currently are now:
 
@@ -174,6 +274,73 @@ Sincerely,
 
 TEMPLATE
 
+
+$self->{'failover_failed_unknown_template'} = <<TEMPLATE;
+
+Greetings [%given_name%] [%last_name%],
+
+The following circuit attempted to fail over to an alternate path, however was not able to. 
+This circuit is currently down.
+
+The details of the circuit as they were before attempted failover:
+
+[%circuit_clr%]
+
+Sincerely,
+
+[%from_signature_name%]
+
+TEMPLATE
+
+$self->{'failover_failed_no_backup_template'} = <<TEMPLATE;
+
+Greetings [%given_name%] [%last_name%],
+
+This circuit was affected by an outage that would cause it to failover, however no alternative path was found.
+The following circuit is currently down. 
+
+The details of the circuit as they were before attempted failover:
+
+[%circuit_clr%]
+
+Sincerely,
+
+[%from_signature_name%]
+
+TEMPLATE
+
+$self->{'failover_failed_path_down_template'} = <<TEMPLATE;
+
+Greetings [%given_name%] [%last_name%],
+
+This circuit attempted to failover, however did not succeed because the alternative path is also down.
+
+The following circuit is currently down. 
+
+The details of the circuit as they were before attempted failover:
+
+[%circuit_clr%]
+
+Sincerely,
+
+[%from_signature_name%]
+
+TEMPLATE
+
+$self->{'return_to_primary_template'} = <<TEMPLATE;
+
+Greetings [%given_name%] [%last_name%],
+
+This circuit has been migrated back to its primary from backup due to the circuit preferences.
+
+The details of the circuit are below:
+
+[%clr%]
+
+Sincerely,
+
+[$from_signature_name%]
+TEMPLATE
 
 }
 
@@ -213,7 +380,7 @@ sub _connect_services {
         realm   => $self->{'realm'},
         usePost => 0
     );
-
+    #warn Dumper ($websvc);
     $self->{'ws'} = $websvc;
     my $db = OESS::Database->new();
     $self->{'db'} = $db;
@@ -222,9 +389,11 @@ sub _connect_services {
 }
 
 sub get_notification_data {
+    
     my $self   = shift;
     my %args = @_;
     my $ws = $self->{'ws'};
+    #warn Dumper($ws);
     my $ckt    = $args{'circuit'};
     my $username;
     
@@ -236,7 +405,7 @@ sub get_notification_data {
     }
     #warn Dumper ($details);
     $user_id = $details->{'user_id'};
-    my $clr = $ws->get_circuit_clr(action=> 'generate_clr', circuit_id => $ckt->{'circuit_id'} )->{'results'}->{'clr'} ;
+    my $clr = $ws->generate_clr(action=> 'generate_clr', circuit_id => $ckt->{'circuit_id'} )->{'results'}->{'clr'} ;
     unless ($clr){
         warn "No CLR for $ckt->{'circuit_id'} ?";
     }
@@ -259,7 +428,13 @@ sub get_notification_data {
           }
     }
 
-    return ({  'username'=> $username, 'last_modified' => $details->{'last_edited'}, 'clr' => $clr, 'affected_users' => $workgroup_members });
+    my $circuit = { 'circuit_id' => $details->{'circuit_id'},
+                    'workgroup_id' => $details->{'workgroup_id'},
+                    'name' => $details->{'name'},
+                    'description' => $details->{'description'},
+                    'clr' => $clr
+                  };
+    return ({  'username'=> $username, 'last_modified' => $details->{'last_edited'}, 'clr' => $clr, 'affected_users' => $workgroup_members, 'circuit'=> $circuit  });
 }
 
 
@@ -277,10 +452,30 @@ sub _send_notification {
                                   Type => 'text/plain',
                                   Data => uri_unescape($body) );
     $message->send('smtp','localhost');
-    
-    
+   
+}
+
+sub notify_failover{
+    my $self = shift;
+    my ($circuit,$success) = @_;
+        
+    my $circuit_notification_data = $self->get_notification_data( circuit => $circuit );   
+    #$circuit->{'clr'} = $circuit_notification_data->{'clr'};
+    #warn "$success";
+    foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
+
+        $self->send_notification( 
+                                           notification_type =>"failover_$success",
+                                           username => $circuit_notification_data->{'username'},
+                                           contact_data => $user,
+                                           circuit_data => $circuit_notification_data->{'circuit'}
+                                          
+                                          );
+    }
+
     
 
 }
+
 
 1;
