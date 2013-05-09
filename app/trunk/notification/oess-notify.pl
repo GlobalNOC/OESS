00eb0777e12cacfbb090a540f9233012a5b3afcc
Grant McNaught <gmcnaugh@grnoc.iu.edu>
2013-05-09 15:55:04 +0000

    ISSUE=5424 removed cruft from oess-notify.pl

--- app/trunk/notification/oess-notify.pl
+++ app/trunk/notification/oess-notify.pl
@@ -1,9 +1,28 @@
 #!/usr/bin/perl
+#------ NDDI OESS Notfication Daemon
+##-----
+##----- $HeadURL:
+##----- $Id:
+##-----
+##----- Daemon listens to events from circuit provisioning,modification, and failovers and sends notifications.
+##---------------------------------------------------------------------
+##
+## Copyright 2013 Trustees of Indiana University
+##
+##   Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##   You may obtain a copy of the License at
+##
+##       http://www.apache.org/licenses/LICENSE-2.0
+##
+##   Unless required by applicable law or agreed to in writing, software
+##   distributed under the License is distributed on an "AS IS" BASIS,
+##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##   See the License for the specific language governing permissions and
+##   limitations under the License.
+#
 
 use strict;
-
-#use lib ("../lib");
-
 use OESS::DBus;
 use OESS::Notification;
 use Proc::Daemon;
@@ -13,159 +32,56 @@ use Data::Dumper;
 
 #OESS Notification Daemon.
 
-#create notification object globally
-#my $notification = OESS::Notification->new(
-
-#                                         );
-
-
 sub connect_to_dbus {
 
-  # my $prov_dbus = OESS::DBus->new(
-   #     service  => 'org.nddi.syslogger',
-   #     instance => '/controller1'
-   # );
-   my $fwdctl_dbus = OESS::DBus->new(
-                                    service =>'org.nddi.fwdctl',
-                                    instance=>'/controller1'
-                                    );
+    my $fwdctl_dbus = OESS::DBus->new(
+        service  => 'org.nddi.fwdctl',
+        instance => '/controller1'
+    );
 
-   my $dbus = OESS::DBus->new(service => 'org.nddi.openflow', instance => '/controller1');
-   
-   ##dbus registers events to listen for on creation / scheduled
-   my $bus = Net::DBus->system;
-   my $service = $bus->export_service("org.nddi.notification");
-   
-   my $notification = OESS::Notification->new(service =>$service,
-                                               config_file=>'/etc/oess/notification.xml');
-   #warn Dumper ($object);
-   #warn Dumper $object->{'ws'};
-   if (!defined $notification){
-        #fuuu
+    my $dbus = OESS::DBus->new(
+        service  => 'org.nddi.openflow',
+        instance => '/controller1'
+    );
+
+    ##dbus registers events to listen for on creation / scheduled
+    my $bus     = Net::DBus->system;
+    my $service = $bus->export_service("org.nddi.notification");
+
+    #OESS::Notification listens on 'org.nddi.notification'
+    my $notification = OESS::Notification->new(
+        service     => $service,
+        config_file => '/etc/oess/database.xml'
+    );
+
+    if ( !defined $notification ) {
+        
         die("could not export org.nddi.notification service");
     }
 
+ #closure allows us to have notification object in scope when fwdctl events fire
+    my $callback = sub {
+        my ($circuit) = @_;
+        $notification->notify_failover($circuit);
+    };
 
-    #if ( defined($prov_dbus) ) {
-     #   $prov_dbus->connect_to_signal( "signal_circuit_provision",   \&notify_provision );
-     #   $prov_dbus->connect_to_signal( "signal_circuit_decommission", \&notify_decomission );
-     #   $prov_dbus->connect_to_signal( "signal_circuit_modify",      \&notify_modification );
-    #}
-    #else {
-        #syslog( LOG_ERR, "Unable to connect to the DBus" );
-     #   die;
-    #}
+    if ( defined($fwdctl_dbus) ) {
+        warn "connecting to signal_circuit_failover signal";
+        $fwdctl_dbus->connect_to_signal( "signal_circuit_failover", $callback );
 
-   my $callback =  sub {
-           my ($circuit) = @_;
-           $notification->notify_failover($circuit);
+    }
+    else {
+        die;
+    }
 
-       };
-   
-   if (defined ($fwdctl_dbus) ){
-       warn "connecting to signal_circuit_failover signal";
-       
-       $fwdctl_dbus->connect_to_signal("signal_circuit_failover", $callback );
-
-   }
-   else {
-       die;
-   }
-
-   $fwdctl_dbus->start_reactor();
+    $fwdctl_dbus->start_reactor();
 
 }
 
-
-
-
-# sub notify_failover{
-#     my ($circuit,$success) = @_;
-        
-#     my $circuit_notification_data = $notification->get_notification_data( circuit => $circuit );   
-#     #$circuit->{'clr'} = $circuit_notification_data->{'clr'};
-#     #warn "$success";
-#     foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
-
-#         $notification->send_notification( 
-#                                            notification_type =>"failover_$success",
-#                                            username => $circuit_notification_data->{'username'},
-#                                            contact_data => $user,
-#                                            circuit_data => $circuit_notification_data->{'circuit'}
-                                          
-#                                           );
-#     }
-
-    
-
-# }
-
-# sub notify_provision{
-#     my ($circuit) = @_;
-
-#     my $circuit_notification_data = $notification->get_notification_data(  circuit =>$circuit );   
-#     $circuit->{'clr'} = $circuit_notification_data->{'clr'};
-#     foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
-
-#         $notification->send_notification( 
-#                                            notification_type =>'provision',
-#                                            username => $circuit_notification_data->{'username'},
-#                                            contact_data => $user,
-#                                            circuit_data => $circuit
-                                           
-#                                           );
-#     }
-
-
-# }
-
-# sub notify_decomission {
-#     my ($circuit) = @_;
-
-#     my $circuit_notification_data = $notification->get_notification_data(  circuit =>$circuit );   
-
-#     foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
-#     $circuit->{'clr'} = $circuit_notification_data->{'clr'};
-#         $notification->send_notification( 
-#                                            notification_type =>'decommission',
-#                                            username => $circuit_notification_data->{'username'},
-#                                            contact_data => $user,
-#                                            circuit_data => $circuit
-                                           
-#                                           );
-#     }
-
-
-
-# }
-
-# sub notify_modification {
-#     my ($circuit) = @_;
-
-#     my $circuit_notification_data = $notification->get_notification_data( circuit => $circuit );   
-#     $circuit->{'clr'} = $circuit_notification_data->{'clr'};
-#     foreach my $user ( @{$circuit_notification_data->{'affected_users'} } ){
-#         $notification->send_notification( 
-#                                            notification_type =>'modify',
-#                                            username => $circuit_notification_data->{'username'},
-#                                            contact_data => $user,
-#                                            circuit_data => $circuit
-                                          
-#                                           );
-#     }
-
-# }
-
-sub main{
+sub main {
     connect_to_dbus;
 }
-#sub fwdctl_failover_callback{
-#    my $notification = shift;
-#    my ($circuit,$success) = @_;
 
-    
-    
-#}
 our ( $opt_f, $opt_u );
 my $result = GetOptions(
     "foreground" => \$opt_f,
@@ -186,8 +102,7 @@ else {
     }
     else {
         $daemon =
-          Proc::Daemon->new(
-            pid_file => '/var/run/oess/oess_ckt_notify.pid' );
+          Proc::Daemon->new( pid_file => '/var/run/oess/oess_ckt_notify.pid' );
     }
     my $kid = $daemon->Init;
 
