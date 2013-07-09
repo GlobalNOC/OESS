cc82f37899920dd85fb0173385b3a0a24f681b49
AJ Ragusa <aragusa@grnoc.iu.edu>
2013-07-09 15:39:34 +0000

    adding the upgrade script and updating the version in the fresh schema

--- perl-lib/OESS-Database/trunk/share/nddi.sql
+++ perl-lib/OESS-Database/trunk/share/nddi.sql
@@ -492,7 +492,7 @@ CREATE TABLE `oess_version` (
 
 LOCK TABLES `oess_version` WRITE;
 /*!40000 ALTER TABLE `oess_version` DISABLE KEYS */;
-INSERT INTO `oess_version` VALUES ('1.0.10');
+INSERT INTO `oess_version` VALUES ('1.0.11');
 /*!40000 ALTER TABLE `oess_version` ENABLE KEYS */;
 UNLOCK TABLES;
 
