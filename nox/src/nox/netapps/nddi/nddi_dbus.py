28ee65214c3563c424d5c43eb4942fa5894730cd
AJ Ragusa <aragusa@grnoc.iu.edu>
2014-01-13 00:52:33 +0000

    ISSUE=7769 fixed the dp_leave event so that it no longer fires and event

--- nox/src/nox/netapps/nddi/nddi_dbus.py
+++ nox/src/nox/netapps/nddi/nddi_dbus.py
@@ -376,7 +376,7 @@ def datapath_leave_callback(sg,dp_id):
     if dp_id in last_flow_stats:
         del last_flow_stats[dp_id]
     if dp_id in flowmod_callbacks:
-        flowmod_callbacks.remove(dp_id)
+        del flowmod_callbacks[dp_id]
 
     sg.datapath_leave(dp_id)
 
