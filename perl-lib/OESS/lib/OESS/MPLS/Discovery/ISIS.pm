49406d8877dc8e95b347ae09bec2a98801d094f7
Jonathan Stout <jonstout@grnoc.iu.edu>
2016-09-16 20:41:07 +0000

    removing function update

--- perl-lib/OESS/lib/OESS/MPLS/Discovery/ISIS.pm
+++ perl-lib/OESS/lib/OESS/MPLS/Discovery/ISIS.pm
@@ -44,7 +44,7 @@ sub process_results{
 	$self->{'logger'}->error("Error fetching current links from the network...");
 	return;
     }
-    my $adjacencies = $self->_process_adjacencies2($isis);
+    my $adjacencies = $self->_process_adjacencies($isis);
 
     return $adjacencies;
 }
@@ -88,60 +88,6 @@ sub _process_adjacencies{
 		$adjacencies->{$node}{$adj->{'remote_system_name'}}{'node_a'}{'interface_name'} = $adj->{'interface_name'};
 		$adjacencies->{$node}{$adj->{'remote_system_name'}}{'node_a'}{'ip_address'} = $adj->{'ip_address'};
                 $adjacencies->{$node}{$adj->{'remote_system_name'}}{'node_a'}{'ipv6_address'} = $adj->{'ipv6_address'};
-	    }
-        }
-    }
-
-    return $adjacencies;
-}
-
-sub _process_adjacencies2{
-    my $self = shift;
-    my $isis = shift;
-    
-    my $adjacencies = {};
-    
-    foreach my $node (keys %{$isis}) {
-	if (!defined $adjacencies->{$node}) {
-	    $adjacencies->{$node} = {};
-	}
-
-        foreach my $adj (@{$isis->{$node}->{'results'}}) {
-
-            my $neighbor = $adj->{'remote_system_name'};
-	    if (!defined $adjacencies->{$neighbor}) {
-		$adjacencies->{$neighbor} = {};
-	    }
-
-	    if (!defined $adjacencies->{$node}{$neighbor}) {
-		$adjacencies->{$node}{$neighbor} = { operational_state => $adj->{'operational_state'},
-                                                     node_a => { node => $node,
-                                                                 interface_name => $adj->{'interface_name'},
-                                                               },
-                                                     node_z => { node         => $neighbor,
-                                                                 ip_address   => $adj->{'ip_address'},
-                                                                 ipv6_address => $adj->{'ipv6_address'}
-                                                               }
-                                                   };
-		
-		$adjacencies->{$neighbor}{$node} = { operational_state => $adj->{'operational_state'},
-                                                     node_a => { node => $neighbor,
-                                                                 ip_address => $adj->{'ip_address'},
-                                                                 ipv6_address => $adj->{'ipv6_address'}
-                                                               },
-                                                     node_z => { node => $node,
-                                                                 interface_name => $adj->{'interface_name'}
-                                                               }
-                                                   };
-	    } else {
-		#we already found it update it with the missing info from the other side!
-                $adjacencies->{$node}{$neighbor}{'node_a'}{'interface_name'} = $adj->{'interface_name'};
-		$adjacencies->{$node}{$neighbor}{'node_z'}{'ip_address'}     = $adj->{'ip_address'};
-                $adjacencies->{$node}{$neighbor}{'node_z'}{'ipv6_address'}   = $adj->{'ipv6_address'};
-
-		$adjacencies->{$neighbor}{$node}{'node_a'}{'ip_address'}     = $adj->{'ip_address'};
-		$adjacencies->{$neighbor}{$node}{'node_a'}{'ipv6_address'}   = $adj->{'ipv6_address'};
-		$adjacencies->{$neighbor}{$node}{'node_z'}{'interface_name'} = $adj->{'interface_name'};
 	    }
         }
     }
