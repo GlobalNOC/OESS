7a66253727fc6dd39454bce22a46a7eae43aac97
Jonathan Stout <jonstout@globalnoc.iu.edu>
2017-07-03 15:41:30 +0000

    setting default mpls value to 0 as in the upgrade scripts

--- perl-lib/OESS/share/nddi.sql
+++ perl-lib/OESS/share/nddi.sql
@@ -377,7 +377,7 @@ CREATE TABLE `node_instantiation` (
   `admin_state` enum('planned','available','active','maintenance','decom') NOT NULL DEFAULT 'planned',
   `dpid` varchar(40) NOT NULL,
   `openflow` int(1) default 1,
-  `mpls` int(1) default 1,
+  `mpls` int(1) default 0,
   `vendor` varchar(255),
   `model` varchar(255),
   `sw_version` varchar(255),
