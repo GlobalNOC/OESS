0430e493f4e86d8e08126b3ffef757423cc9937b
Jonathan Stout <jonstout@grnoc.iu.edu>
2018-05-18 17:40:17 +0000

    adding initial create private network form

---
+++ frontend/www/js_templates/api/interfaces.js
@@ -0,0 +1,24 @@
+/**
+ * getInterfaces returns a list of all interfaces on nodeName that may
+ * be used for provisioning by workgroupID.
+ *
+ * @param {integer} workgroupID - Identifier of the current workgroup
+ * @param {string} nodeName - Node used to filter interfaces
+ * @param {integer} [trunk=1] - Include trunk interfaces
+ */
+async function getInterfaces(workgroupID, nodeName, trunk=1) {
+  // Encode added to align with original source.
+  nodeName = encodeURIComponent(nodeName);
+
+  let url = `services/data.cgi?method=get_node_interfaces&node=${nodeName}&workgroup_id=${workgroupID}&show_down=1&show_trunk=${trunk}`;
+
+  try {
+    const resp = await fetch(url, {method: 'get', credentials: 'include'});
+    const data = await resp.json();
+    return data.results;
+  } catch(error) {
+    console.log('Failure occurred in get_node_interfaces.');
+    console.log(error);
+    return [];
+  }
+}
