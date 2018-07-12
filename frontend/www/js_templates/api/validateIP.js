7c3b22b7238cb7602f54f91173c1335357675cca
Jonathan Stout <jonstout@globalnoc.iu.edu>
2018-07-12 14:01:32 +0000

    adding regexs for ipv4 or ipv6 validation

--- frontend/www/js_templates/api/validateIP.js
+++ frontend/www/js_templates/api/validateIP.js
@@ -8,13 +8,19 @@ let IPv6 = '^((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){
 let IPv6Placeholder = 'fe80:0000:0000:0000:0204:61ff:fe9d:f156';
 
 let IPv6CIDR = '^((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?(\/([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-8]))$';
-let IPv6CIDRPlaceholder = 'fe80:0000:0000:0000:0204:61ff:fe9d:f156/64'
+let IPv6CIDRPlaceholder = 'fe80:0000:0000:0000:0204:61ff:fe9d:f156/64';
+
+let IPv4orIPv6 = '([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?$)';
+let IPv4orIPv6Placeholder = '192.168.1.1 or fe80:0000:0000:0000:0204:61ff:fe9d:f156';
+
+let IPv4CIDRorIPv6CIDR = '(^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$|^((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?(\/([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-8]))$)';
+let IPv4CIDRorIPv6CIDRPlaceholder = '192.168.1.1/24 or fe80:0000:0000:0000:0204:61ff:fe9d:f156/64';
 
 function asIPv4(element) {
   element.setAttribute('type', 'text');
   element.setAttribute('pattern', IPv4);
   element.setAttribute('placeholder', IPv4Placeholder);
-  
+
   element.addEventListener('input', function(e) {
     if (element.validity.valueMissing) {
       element.setCustomValidity('Please fill out this field.');
@@ -30,7 +36,7 @@ function asIPv4CIDR(element) {
   element.setAttribute('type', 'text');
   element.setAttribute('pattern', IPv4CIDR);
   element.setAttribute('placeholder', IPv4CIDRPlaceholder);
-  
+
   element.addEventListener('input', function(e) {
     if (element.validity.valueMissing) {
       element.setCustomValidity('Please fill out this field.');
@@ -46,7 +52,7 @@ function asIPv6(element) {
   element.setAttribute('type', 'text');
   element.setAttribute('pattern', IPv6);
   element.setAttribute('placeholder', IPv6Placeholder);
-  
+
   element.addEventListener('input', function(e) {
     if (element.validity.valueMissing) {
       element.setCustomValidity('Please fill out this field.');
@@ -62,7 +68,7 @@ function asIPv6CIDR(element) {
   element.setAttribute('type', 'text');
   element.setAttribute('pattern', IPv6CIDR);
   element.setAttribute('placeholder', IPv6CIDRPlaceholder);
-  
+
   element.addEventListener('input', function(e) {
     if (element.validity.valueMissing) {
       element.setCustomValidity('Please fill out this field.');
@@ -74,3 +80,34 @@ function asIPv6CIDR(element) {
   }, false);
 }
 
+function asIPv4orIPv6(element) {
+  element.setAttribute('type', 'text');
+  element.setAttribute('pattern', IPv4orIPv6);
+  element.setAttribute('placeholder', IPv4orIPv6Placeholder);
+
+  element.addEventListener('input', function(e) {
+    if (element.validity.valueMissing) {
+      element.setCustomValidity('Please fill out this field.');
+    } else if (element.validity.patternMismatch) {
+      element.setCustomValidity('Please input a valid IPv4 or IPv6 address.');
+    } else {
+      element.setCustomValidity('');
+    }
+  }, false);
+}
+
+function asIPv4CIDRorIPv6CIDR(element) {
+  element.setAttribute('type', 'text');
+  element.setAttribute('pattern', IPv4CIDRorIPv6CIDR);
+  element.setAttribute('placeholder', IPv4CIDRorIPv6CIDRPlaceholder);
+
+  element.addEventListener('input', function(e) {
+    if (element.validity.valueMissing) {
+      element.setCustomValidity('Please fill out this field.');
+    } else if (element.validity.patternMismatch) {
+      element.setCustomValidity('Please input a valid IPv4 or IPv6 subnet in CIDR notation.');
+    } else {
+      element.setCustomValidity('');
+    }
+  }, false);
+}
