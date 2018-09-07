3f1ba7d2831716de7609837497ca4219e9c260ac
Michael Johnson <mj82@grnoc.iu.edu>
2018-09-07 14:50:59 +0000

    Begin traversing virtual interface datastructures #617

--- app/oess_pull_aws_interfaces.pl
+++ app/oess_pull_aws_interfaces.pl
@@ -10,8 +10,15 @@ use JSON;
 use Log::Log4perl;
 use Paws;
 use XML::Simple;
+#use MooseX::Storage;
+#    with Storage('format' => 'JSON');
+    #with Storage('format' => 'JSON', 'io' => 'File');
+
 
 my $self;
+
+my $jsonObj = JSON->new->allow_nonref->convert_blessed->allow_blessed;
+
 
 sub new {
     #my $class = shift;
@@ -46,11 +53,74 @@ new();
 
 # DescribeVirtualInterfaces
 
-   my $resp = $dc->DescribeVirtualInterfaces(
+    my $resp = $dc->DescribeVirtualInterfaces();
+
+    my $virtual_interfaces = $resp->{'VirtualInterfaces'};
+    warn "interfaces \n";
+    #my $json = $resp->freeze();
+    #warn $json->encode( $virtual_interfaces );
+
+    warn "json " . Dumper ( $resp );
+
+    for my $intf ( @$virtual_interfaces ) {
+        my $bgp_peers = $intf->BgpPeers;
+        #warn "bgp peers " . Dumper @{ $interface->BgpPeers }[0]->AddressFamily;
+
+        my @fields = (
+            'Vlan', 
+            'VirtualInterfaceState', 
+            'ConnectionId',
+            'AmazonAddress',
+            'Asn',
+            'OwnerAccount',
+            'CustomerAddress',
+            'Location',
+            'AmazonSideAsn',
+            'DirectConnectGatewayId',
+            'VirtualInterfaceType',
+            'VirtualInterfaceName',
+            'VirtualInterfaceId',
+            'AddressFamily',
+            'AuthKey',
+            'VirtualGatewayId',
+
+        );
+
+        my $results = {
+            #vlan => $intf->Vlan,
+            #state => $intf->VirtualInterfaceState,
+            #connection_id => $intf->ConnectionId,
+            #amazon_address => $intf->AmazonAddress
 
 
-    );
+            
 
-    warn "interfaces " . Dumper $resp;
+        };
+        for my $field ( @fields ) {
+            $results->{$field} = $intf->{$field};
+
+        }
+        warn "RESULTS\n" . Dumper $results;
+
+
+
+               #'Vlan' => 101,
+               #'VirtualInterfaceState' => 'available',
+               #'ConnectionId' => 'dxcon-fgm77851',
+               #'AmazonAddress' => '169.254.255.1/30',
+               #'Asn' => 55038,
+               #'OwnerAccount' => '347957162513',
+               #'CustomerAddress' => '169.254.255.2/30',
+               #'Location' => 'EqDC2',
+               #'AmazonSideAsn' => 7224,
+               #'DirectConnectGatewayId' => '',
+               #'VirtualInterfaceType' => 'private',
+               #'VirtualInterfaceName' => 'Test L3VPN Interface',
+               #'VirtualInterfaceId' => 'dxvif-fgtrfa3c',
+               #'AddressFamily' => 'ipv4',
+               #'AuthKey' => '0xqpdZjNOHf0hZUC0owwSLUI',
+               #'VirtualGatewayId' => 'vgw-64ce390d'
+
+    }
 
 
