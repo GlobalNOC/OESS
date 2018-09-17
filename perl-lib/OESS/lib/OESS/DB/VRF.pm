26efa79761783df8e2a20b8d4f90b7d06920563d
Jonathan Stout <jonstout@globalnoc.iu.edu>
2018-09-17 15:06:00 +0000

    adding check in get_vrfs to handle workgroups that own no interfaces
    When a workgroup that owned zero interfaces was used to filter vrfs,
    an invalid sql query was generated. We now use a secondary query
    string, when this case is encountered.

--- perl-lib/OESS/lib/OESS/DB/VRF.pm
+++ perl-lib/OESS/lib/OESS/DB/VRF.pm
@@ -342,9 +342,14 @@ sub get_vrfs{
                 $vals .= ",?";
             }
         }
-        push(@where_str, "(workgroup_id = ? or vrf_ep.interface_id in ($vals))");
+        if (!(scalar @$interfaces)) {
+            push(@where_str, "(workgroup_id = ?");
+        } else {
+            push(@where_str, "(workgroup_id = ? or vrf_ep.interface_id in ($vals))");
+        }
+        warn Dumper(@where_str);
     }
-    
+
     my $where;
     foreach my $str (@where_str){
         if(!defined($where)){
