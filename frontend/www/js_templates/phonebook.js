f6ff05c69b4e401487dba8b42953e80063ae0cca
Paritosh Morparia <pmorpari@pmorpari-dev.grnoc.iu.edu>
2019-02-08 16:09:15 +0000

    Add contact

--- frontend/www/js_templates/phonebook.js
+++ frontend/www/js_templates/phonebook.js
@@ -53,7 +53,10 @@ async function loadEntityList(parentEntity=null) {
     let entitiesList = document.querySelector('#entity-list');
  
     let edit_entity_btn = document.querySelector('#edit-entity');
+    let add_user_btn = document.querySelector('#user-dropdown');
+
     edit_entity_btn.style.display = 'none';
+    add_user_btn.style.display = 'none';
 
     let logoURL = entity.logo_url || '../media/default_entity.png';
     let description = entity.description;
@@ -168,26 +171,91 @@ async function loadEntityList(parentEntity=null) {
         document.querySelector('#entity-contacts-title').style.display = 'block';
     }
 
-    let entityContacts = '';
-    entity.contacts.forEach(function(contact) {
-            entityContacts += `<p class="entity-contact"><b>${contact.first_name} ${contact.last_name}</b><br/>${contact.email}</p>`;
-    });
-    document.querySelector('#entity-contacts').innerHTML = entityContacts;
-
     let user = await getCurrentUser();
     let url2 = `[% path %]services/entity.cgi?action=get_valid_users&entity_id=${entityID}`;
     const resp = await fetch(url2, {method: 'get', credentials: 'include'}); 
     const data = await resp.json();
-    if (data.results.includes(user.user_id)){
+    var valid_users = data.results;
+    let entityContacts = '';
+    let contact_ids = [];
+    entity.contacts.forEach(function(contact) {
+            console.log("contact");
+            console.log(contact);
+            var user_id = contact.user_id;
+            contact_ids.push(user_id);
+	    entityContacts += `<p class="entity-contact"><b>${contact.first_name} ${contact.last_name}</b>`;
+            if (valid_users.includes( user.user_id)){
+              entityContacts += `<sup style = "cursor:pointer" onclick='remove_user(${user_id}, ${entity.entity_id})'> &#10006</sup>`;
+            }
+            
+            entityContacts += `<br/>${contact.email}<br/></p>`;
+    });
+    document.querySelector('#entity-contacts').innerHTML = entityContacts;
+
+    if (valid_users.includes(user.user_id)){
           let edit_entity_btn = document.querySelector('#edit-entity');
-          edit_entity_btn.style.display = 'block';
+          edit_entity_btn.style.display = 'inline-block';
           edit_entity_btn.onclick = function(){
-          window.location.href = `[% path %]new/index.cgi?action=edit_entity&entity_id=${entityID}`;
-        };
-   
+            window.location.href = `[% path %]new/index.cgi?action=edit_entity&entity_id=${entityID}`;
+          };
+
+          let user_list = document.querySelector('#user-list');
+          user_list.innerHTML ="";
+          add_user_btn.style.display = 'block';
+          let users_url = `[% path %]services/data.cgi?action=get_users`;
+          const users_resp = await fetch(users_url, {method:'get', credentials:'include'});
+          var users = await users_resp.json();
+          users = users['results'];
+          console.log(users);
+          let i =0;
+          for (i =0 ; i < users.length; i++){
+            var ele = document.createElement("A");
+            let user_id = users[i]['user_id'];
+            if (!contact_ids.includes(user_id)){
+              ele.onclick = function(){ add_user(user_id, entityID)};
+              var t = document.createTextNode(users[i]['first_name'] + " "  +users[i]['family_name']);
+              ele.appendChild(t);
+              user_list.appendChild(ele);
+              console.log(users[i]['first_name']);
+            }
+          }
    }
-
-
 }
 
+function showDropdown() {
+  document.getElementById("user-list").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("user-search");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("user-list");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+// Add user to entity
+async function add_user(user_id, entityID){
+  const url = `[% path %]services/entity.cgi?action=add_user&entity_id=${entityID}&user_id=${user_id}`;
+  const response = await fetch(url, {method:'get', credentials:'include'});
+  const data = await response.json();
+  console.log(data);
+  await loadEntityList(entityID);
+ }
+
+async function remove_user(user_id, entityID){
+  const url = `[% path %]services/entity.cgi?action=remove_user&entity_id=${entityID}&user_id=${user_id}`;
+  const response = await fetch(url, {method:'get', credentials:'include'});
+  const data = await response.json;
+
+  await loadEntityList(entityID);
+}
 
