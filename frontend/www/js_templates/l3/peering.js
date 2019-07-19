eb71d884b13c0a4184a00bba00d6bc331ab5dcf8
Jonathan Stout <jonstout@globalnoc.iu.edu>
2019-07-19 21:17:08 +0000

    removing excess ui code

--- frontend/www/js_templates/l3/peering.js
+++ frontend/www/js_templates/l3/peering.js
@@ -1,28 +1,3 @@
-class Peering {
-  constructor(props) {
-    this.props = props;
-  }
-
-  render() {
-    return `
-    <tr>
-      <td>${this.props.ipVersion === 4 ? 'ipv4' : 'ipv6'}</td>
-      <td>${this.props.asn}</td>
-      <td>${this.props.yourPeerIP}</td>
-      <td>${this.props.cloudAccountType ? '*****' : this.props.key}</td>
-      <td>${this.props.oessPeerIP}</td>
-      <td>
-        <button class="btn btn-danger btn-sm"
-                type="button"
-                onclick="deletePeering(${this.props.endpointIndex}, ${this.props.index})">
-          &nbsp;<span class="glyphicon glyphicon-trash"></span>&nbsp;
-        </button>
-      </td>
-    </tr>
-    `;
-  }
-}
-
 class Peering2 {
   constructor(parent, peering) {
     let template = document.querySelector('#template-l3-peering2');
