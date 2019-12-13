248a989db0cad5ff1ebf10a9f8b38fa2d08774f8
Roja Raman <roja_raman@outlook.com>
2019-12-13 20:35:23 +0000

    Nav Bar links fixed

--- frontend/www/new/admin_new/components/nav_bar/NavBrand.jsx
+++ frontend/www/new/admin_new/components/nav_bar/NavBrand.jsx
@@ -10,8 +10,12 @@ export default class NavBrand extends React.Component{
         }	
 	
 	render(){
+
+		const textstyle = {
+			fontSize: '2em'
+		};
 		return (
-      		<a className="navbar-brand" href={this.props.linkTo}>
+      		<a className="navbar-brand" style={textstyle} href={this.props.linkTo}>
 		<img className="navbar-brand-logo" src={this.props.src}/>	
 		{this.props.text}
 		</a>
