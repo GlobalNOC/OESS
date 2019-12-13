3f4ab910d4e1a3557b1b0e161bea6167e8df8053
Roja Raman <roja_raman@outlook.com>
2019-12-13 20:35:23 +0000

    Nav Bar links fixed

--- frontend/www/new/admin_new/components/nav_bar/NavBar.jsx
+++ frontend/www/new/admin_new/components/nav_bar/NavBar.jsx
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import NavBrand from './NavBrand.jsx';
 import NavMenu from './NavMenu.jsx';
+import NavMenuRight from './NavMenuRight.jsx';
 import { testConfig } from '../.././test.jsx';
 
 export default class NavBar extends React.Component{
 	constructor(props) {
         	super(props);
-        	this.state = {}
+        	this.state = {
+		}
 	}
 
 	render(){
 
 	var navbar = {};
 	let path = testConfig.user;
-	navbar.brand = {linkTo: "#", src: path+'media/internet2-logo.png', text: "Cloud Connect"};
+	navbar.brand = {linkTo: path+"new/index.cgi", src: path+'media/internet2-logo.png', text: "Cloud Connect"};
 	navbar.links = [
 		{dropdown: true, text: "New Connections", links: [
-   			 {linkTo: "#", text: "Layer 1"},
-    			 {linkTo: "#", text: "Layer 2"}
+   			 {linkTo: path+"new/index.cgi?action=provision_l2vpn", text: "Layer 1"},
+    			 {linkTo: path+"new/index.cgi?action=provision_cloud", text: "Layer 2"}
   		]},
-  		{linkTo: "#", text: "Explore"},
-		{linkTo: "#", text: "Workgroup"},
+  		{linkTo: path+"new/index.cgi?action=phonebook", text: "Explore"},
+		{linkTo: path+"new/index.cgi?action=acl", text: "Workgroup"}
+	];
+	navbar.admin = [
+		{linkTo: "#", is_admin: true, text: "Admin"}
+	]
+	navbar.login = [
+		
 	];
 
 
@@ -38,6 +46,7 @@ export default class NavBar extends React.Component{
           </div>
           <div className="collapse navbar-collapse" id="navbar-collapse">
             <NavMenu links={navbar.links} />
+	    <NavMenuRight links={navbar.admin} />
           </div>
         </div>
       </nav>
--- frontend/www/new/admin_new/components/nav_bar/NavLink.jsx
+++ frontend/www/new/admin_new/components/nav_bar/NavLink.jsx
@@ -7,14 +7,19 @@ export default class NavLink extends React.Component{
 constructor(props) {
                 super(props);
                 this.state = {}
-        }
+}
 
 render(){
-
+	let list;
+ 	if(this.props.is_admin == true){
+                list = <li className="admin-link"><a href={this.props.linkTo}>Admin</a></li>;
+        }
+        else{
+                list =  <li><a href={this.props.linkTo}>{this.props.text}</a></li>;
+        }	
 return(
-      <li className={(this.props.active ? "active" : "")}><a href={this.props.linkTo}>{this.props.text}</a></li>
-    );
-
+	list
+	);
 }
 
 
