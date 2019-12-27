e549de799f988137bb9932039cd67169fbf85b60
Roja Raman <roja_raman@outlook.com>
2019-12-27 20:11:48 +0000

    Edit form fixes

--- frontend/www/new/admin_new/components/generic_components/ModalTemplate.jsx
+++ frontend/www/new/admin_new/components/generic_components/ModalTemplate.jsx
@@ -10,25 +10,50 @@ export default class ModalTemplate extends Component {
 constructor(props) {
     super(props);
     console.log("props", props);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
-      show: false,
-      rowdata: null
+	firstname : "",
+	lastname : "",
+	email : "",
+	username : ""	
+	
     };
+    
   }
 
 componentWillReceiveProps(nextProps, prevState) {
  	this.setState({
- 	   show: nextProps.isVisible[0],
- 	   rowdata: nextProps.isVisible[1]
+ 	   firstname: nextProps.rowdata["First Name"],
+	   lastname: nextProps.rowdata["Last Name"],
+	   email: nextProps.rowdata["Email Address"],
+           username: nextProps.rowdata["Username"]
  	 })
  }
+ComponentDidMount() {
+   const { firstname, lastname, email, username  } = this.props.rowdata;
+   this.setState({ firstname, lastname, email, username });
+}
 
+handleChange(event) { 
+        const target = event.target;
+        const name = target.name;        
+        var value = target.value;         
 
-
+        if(name === "firstname")            
+            this.setState({firstname: value});     
+        if(name === "lastname")                         
+            this.setState({lastname: value});
+	if(name === "email")                         
+            this.setState({email: value});
+	if(name === "username")                         
+            this.setState({username: value});
+    }
 
   render() {
-  var rowdata = this.state.rowdata;
+  var currcomponent = this;
+  var rowdata = this.state.username;
   console.log("here data", JSON.stringify(rowdata));
+  
   if(rowdata){
   return(
 	<div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -43,37 +68,37 @@ componentWillReceiveProps(nextProps, prevState) {
                   				<div className="form-group">
                     					<label  className="col-sm-2 control-label modal-label" htmlFor="firstname">First Name</label>
                     					<div className="col-sm-10">
-                        					<input type="text" className="form-control" id="firstname" placeholder="FirstName" value = {this.state.rowdata["First Name"]}/>
+                        					<input type="text" name="firstname"  className="form-control" id="firstname" placeholder="FirstName" value = {this.state.firstname} onChange={this.handleChange}/>
                     					</div>
                   				</div>
                   				<div className="form-group">
                     					<label className="col-sm-2 control-label modal-label" htmlFor="lastname" >Last Name</label>
                     					<div className="col-sm-10">
-                        					<input type="text" className="form-control" id="lastname" placeholder="Last Name" value={this.state.rowdata["Last Name"]}/>
+                        					<input type="text" className="form-control" id="lastname" name="lastname"  placeholder="Last Name" value={this.state.lastname} onChange={this.handleChange}/>
                     					</div>
                   				</div>
 						<div className="form-group">
                                                         <label className="col-sm-2 control-label modal-label" htmlFor="email" >Email Address</label>
                                                         <div className="col-sm-10">
-                                                                <input type="text" className="form-control" id="email" placeholder="email" value={this.state.rowdata["Email Address"]}/>
+                                                                <input type="text" className="form-control" id="email" placeholder="email" value={this.state.email} name="email" onChange={this.handleChange}/>
                                                         </div>
                                                 </div>
 						<div className="form-group">
                                                         <label className="col-sm-2 control-label modal-label" htmlFor="username" >Username(s) (comma separated)</label>
                                                         <div className="col-sm-10">
-                                                                <input type="text" className="form-control" id="username" placeholder="UserName" value={this.state.rowdata["Username"]}/>
+                                                                <input type="text" className="form-control" id="username" placeholder="UserName" value={this.state.username} name="username" onChange={this.handleChange}/>
                                                         </div>
                                                 </div>
 						<div className="form-group">
                                                         <label className="col-sm-2 control-label modal-label" htmlFor="usertype" >User Type</label>
-							<select class="form-control modal-select" id="usertype">
+							<select className="form-control modal-select" id="usertype">
         							<option>Normal</option>
         							<option>Read-Only</option>
       							</select>
                                                 </div>
 						<div className="form-group">
                                                         <label className="col-sm-2 control-label modal-label" htmlFor="status" >Status</label>
-                                                        <select class="form-control modal-select" id="status">
+                                                        <select className="form-control modal-select" id="status">
                                                                 <option>Active</option>
                                                                 <option>Decom</option>
                                                         </select>
