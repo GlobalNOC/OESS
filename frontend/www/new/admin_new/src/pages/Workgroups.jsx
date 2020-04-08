c9b8e877726f16eb92b43ace0a92457501f7e5a6
Jonathan Stout <jonstout@globalnoc.iu.edu>
2020-04-08 20:58:13 +0000

    add initial workgroups page

--- frontend/www/new/admin_new/src/pages/Workgroups.jsx
+++ frontend/www/new/admin_new/src/pages/Workgroups.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import UsersTable from "../user_table/UserTable.jsx";
-import getCurrentUser from '../../api/user_menu.jsx';
-import ModalTemplate2 from '../generic_components/ModalTemplate2.jsx';
-import { PageContextProvider } from '../../contexts/PageContext.jsx';
-import NavBar from "../nav_bar/NavBar.jsx";
 
-import "../../style.css";
+import getCurrentUser from '../api/user_menu.jsx';
 
-class UserLandingPage extends React.Component {
-	
+import ModalTemplate2 from '../components/generic_components/ModalTemplate2.jsx';
+import NavBar from "../components/nav_bar/NavBar.jsx";
+import UsersTable from "../components/user_table/UserTable.jsx";
+
+import { PageContextProvider } from '../contexts/PageContext.jsx';
+
+import "../style.css";
+
+
+class Workgroups extends React.Component {
   constructor(props){
 	super(props);
 	this.state={
@@ -38,24 +41,24 @@ class UserLandingPage extends React.Component {
               <li role="presentation"><a href="#">Devices</a></li>
               <li role="presentation"><a href="#">Links</a></li>
               <li role="presentation"><a href="#">Remote</a></li>
-              <li role="presentation" className="active"><a href="#">Users</a></li>
-              <li role="presentation"><a href="workgroups.html">Workgroups</a></li>
+              <li role="presentation"><a href="index.html">Users</a></li>
+              <li role="presentation" className="active"><a href="#">Workgroups</a></li>
             </ul>
           </div>
 
           <div className="oess-page-content">
             <div>
-              <p className="title"><b>Users</b></p>
-              <p className="subtitle">Add, remove, or update users.</p>
+              <p className="title"><b>Workgroups</b></p>
+              <p className="subtitle">Add, remove, or update Workgroups.</p>
             </div>
             <br />
 
             <form id="user_search_div" className="form-inline">
               <div className="form-group">
-                <input type="text" className="form-control" id="user_search" placeholder="Username"/>
+                <input type="text" className="form-control" id="user_search" placeholder="Workgroup"/>
               </div>
               <button type="button" className="btn btn-primary" data-target="#myModal2" data-toggle="modal">Search</button>
-              <button type="button" className="btn btn-default" data-target="#myModal2" data-toggle="modal">Add User</button>
+              <button type="button" className="btn btn-default" data-target="#myModal2" data-toggle="modal">Add Workgroup</button>
             </form>
             <br />
 
@@ -68,5 +71,5 @@ class UserLandingPage extends React.Component {
   }
 }
 
-let mountNode = document.getElementById("show-users");
-ReactDOM.render(<UserLandingPage />, mountNode);
+let mountNode = document.getElementById("app");
+ReactDOM.render(<Workgroups />, mountNode);
