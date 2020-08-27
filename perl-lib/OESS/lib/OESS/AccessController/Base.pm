ee59f7e396c8cbb1a82ee09e552e9e57f1b055aa
Jonathan Stout <jonstout@globalnoc.iu.edu>
2020-08-27 01:06:26 +0000

    add AccessController for user / workgroup backend

---
+++ perl-lib/OESS/lib/OESS/AccessController/Base.pm
@@ -0,0 +1,33 @@
+package OESS::AccessController::Base;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $args  = {
+        @_
+    };
+
+    return bless $args, $class;
+}
+
+sub create_user { return; }
+sub delete_user { return; }
+sub edit_user { return; }
+sub get_user { return; }
+sub get_users { return; }
+sub get_workgroup_users { return; }
+
+sub create_workgroup { return; }
+sub delete_workgroup { return; }
+sub edit_workgroup { return; }
+sub get_workgroup { return; }
+sub get_workgroups { return; }
+sub get_user_workgroups { return; }
+
+sub add_workgroup_user { return; }
+sub modify_workgroup_user { return; }
+sub remove_workgroup_user { return; }
+
+return 1;
