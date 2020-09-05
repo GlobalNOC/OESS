5876edf3ec37d95bcfb9701150756724bd0dcf18
Jonathan Stout <jonstout@globalnoc.iu.edu>
2020-09-05 04:00:49 +0000

    add default errors to Grouper AccessController

--- perl-lib/OESS/lib/OESS/AccessController/Grouper.pm
+++ perl-lib/OESS/lib/OESS/AccessController/Grouper.pm
@@ -12,21 +12,30 @@ sub new {
     return bless $args, $class;
 }
 
-sub create_user { return; }
-sub delete_user { return; }
-sub edit_user { return; }
+sub create_user {
+    return (undef, "create_user is not supported.");
+}
+
+sub delete_user {
+    return (undef, "delete_user is not supported.");
+}
+
+sub edit_user {
+    return (undef, "edit_user is not supported.");
+}
+
 sub get_user { return; }
 sub get_users { return; }
-sub get_workgroup_users { return; }
+sub get_user_workgroups { return; }
 
 sub create_workgroup { return; }
 sub delete_workgroup { return; }
 sub edit_workgroup { return; }
 sub get_workgroup { return; }
 sub get_workgroups { return; }
-sub get_user_workgroups { return; }
 
 sub add_workgroup_user { return; }
+sub get_workgroup_users { return; }
 sub modify_workgroup_user { return; }
 sub remove_workgroup_user { return; }
 
