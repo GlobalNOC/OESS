cee4bb772cf87bf81f997406d93e207a8a472852
Jonathan Stout <jonstout@globalnoc.iu.edu>
2021-05-13 17:54:12 +0000

    add changes to run oess within docker

--- perl-lib/OESS/lib/OESS/MPLS/Discovery/Paths.pm
+++ perl-lib/OESS/lib/OESS/MPLS/Discovery/Paths.pm
@@ -120,6 +120,7 @@ sub _process_paths{
         my @ckt_path = map { $links->{$_} } keys(%$links);
 
         my $ckt = new OESS::L2Circuit(db => $self->{db2}, circuit_id => $circuit_id);
+        next if !defined $ckt;
         $ckt->load_paths;
 
         my $pri = $ckt->path(type => 'primary');
--- perl-lib/OESS/share/nddi.sql
+++ perl-lib/OESS/share/nddi.sql
@@ -547,6 +547,7 @@ CREATE TABLE `network` (
 --
 -- Table structure for table `node`
 --
+INSERT INTO `network` (`network_id`,`name`,`longitude`,`latitude`,`is_local`) VALUES (1,'oess',0,0,1);
 
 DROP TABLE IF EXISTS `node`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -768,6 +769,7 @@ CREATE TABLE `remote_auth` (
   CONSTRAINT `user_auth_values_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `remote_auth` (`auth_name`,`user_id`) VALUES ('admin',1);
 
 --
 -- Dumping data for table `remote_auth`
@@ -881,6 +883,7 @@ CREATE TABLE `user` (
   KEY `user_idx` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `user` (`user_id`,`email`,`given_names`,`family_name`,`is_admin`,`status`) VALUES (1,'admin@localhost','admin','admin',1,'active');
 
 --
 -- Table structure for table `user_entity_membership`
@@ -896,6 +899,7 @@ CREATE TABLE `user_entity_membership` (
   KEY `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `user_entity_membership` (`user_id`,`entity_id`) VALUES (1,1);
 
 --
 -- Table structure for table `user_workgroup_membership`
@@ -914,6 +918,7 @@ CREATE TABLE `user_workgroup_membership` (
   CONSTRAINT `workgroups_user_workgroup_membership_fk` FOREIGN KEY (`workgroup_id`) REFERENCES `workgroup` (`workgroup_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `user_workgroup_membership` (`workgroup_id`,`user_id`,`role`) VALUES (1,1,'admin');
 
 --
 -- Table structure for table `vrf`
@@ -1043,6 +1048,7 @@ CREATE TABLE `workgroup` (
   UNIQUE KEY `workgroups_idx` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `workgroup` (`workgroup_id`,`description`,`name`,`type`) VALUES (1,'admin','admin','admin');
 
 --
 -- Dumping data for table `workgroup`
