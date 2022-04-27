8f410058b2d0f2d876dab55c0665f9cd9a6855be
Jonathan Stout <jonstout@globalnoc.iu.edu>
2022-04-27 17:22:00 -0400

    sign http requests to oracle

--- perl-lib/OESS/lib/OESS/Cloud/Oracle.pm
+++ perl-lib/OESS/lib/OESS/Cloud/Oracle.pm
@@ -3,9 +3,16 @@ package OESS::Cloud::Oracle;
 use strict;
 use warnings;
 
+use Crypt::OpenSSL::RSA;
 use Data::Dumper;
+use Digest::SHA qw(sha256_base64);
+use HTTP::Date;
+use HTTP::Request;
 use JSON::XS;
 use Log::Log4perl;
+use LWP::UserAgent;
+use MIME::Base64;
+use OESS::Config;
 
 
 =head1 OESS::Cloud::Oracle
@@ -29,16 +36,142 @@ sub new {
     };
     my $self = bless $args, $class;
 
+    if (!defined $self->{config_obj}) {
+        $self->{config_obj} = new OESS::Config(config_filename => $self->{config});
+    }
+
+    # We intercept and sign every request sent using $self->{conn} via the
+    # request_send handler. https://metacpan.org/pod/LWP::UserAgent#add_handler
+    #
+    # TODO Create a conn per interconnect. This probably isn't required, but
+    # it should be done to be consistant with how we handled auth for the
+    # other cloud providers.
+    $self->{conn} = LWP::UserAgent->new();
+    $self->{conn}->add_handler(request_send => sub {
+        my($request, $ua, $handler) = @_;
+
+        my $signing_headers = join(" ", @{$self->_get_signing_headers($request)});
+        my $signature = $self->_compute_signature($request);
+
+        my $config = $self->{config_obj}->oracle()->{CrossConnect1};
+        my $key_id = "$config->{tenancy}/$config->{user}/$config->{fingerprint}";
+
+        my $header = "Signature version=\"1\",headers=\"$signing_headers\",keyId=\"$key_id\",algorithm=\"rsa-sha256\",signature=\"$signature\"";
+
+        $request->header('Content-Type' => 'application/json; charset=UTF-8');
+        $request->header(Authorization => $header);
+
+        return;
+    });
+
     return $self;
+}
+
+=head2 _get_signing_headers
+
+Returns list of headers for the HTTP method defined under Required Headers at
+https://docs.oracle.com/en-us/iaas/Content/API/Concepts/signingrequests.htm
+
+=cut
+sub _get_signing_headers {
+    my $self = shift;
+    my $req  = shift;
+
+    # Ref: https://github.com/oracle/oci-go-sdk/blob/master/common/http_signer.go#L46-L52
+    if ($req->{_method} eq 'POST' || $req->{_method} eq 'PUT' || $req->{_method} eq 'PATCH') {
+        return ["date", "(request-target)", "host", "content-length", "content-type", "x-content-sha256"];
+    } else {
+        return ["date", "(request-target)", "host"];
+    }
+}
+
+=head2 _get_signing_string
+
+Returns string to sign composed of headers defined under Required Headers at
+https://docs.oracle.com/en-us/iaas/Content/API/Concepts/signingrequests.htm
+
+    date: Wed, 27 Apr 2022 16:10:45 GMT\n(request-target): get /20160918/virtualCircuits?key=value\nhost: iaas.us-ashburn-1.oraclecloud.com
+
+=cut
+sub _get_signing_string {
+    my $self = shift;
+    my $req  = shift;
+
+    my $signing_headers = $self->_get_signing_headers($req);
+    my $signing_parts = [];
+
+    foreach my $part (@$signing_headers) {
+        my $value;
+        if ($part eq '(request-target)') {
+            my $method = lc($req->{_method});
+            $req->uri =~ m/\.com(.*)/;
+            $value = "$method $1"; # Ex: get /20160918/virtualCircuits
+        }
+        elsif ($part eq 'host') {
+            $value = 'iaas.us-ashburn-1.oraclecloud.com';
+        }
+        elsif ($part eq 'date') {
+            $value = time2str(time);
+            $req->header(date => $value); # Must be in request headers
+        }
+        elsif ($part eq 'content-type') {
+            $value = 'application/json';
+            $req->header('content-type' => $value);
+        }
+        elsif ($part eq 'content-length') {
+            $value = length($req->content);
+            $req->header('content-length' => $value);
+        }
+        elsif ($part eq 'x-content-sha256') {
+            $value = sha256_base64($req->content);
+            # Add required padding
+            # https://perldoc.perl.org/Digest::SHA#PADDING-OF-BASE64-DIGESTS
+            while (length($value) % 4) {
+                $value .= '=';
+            }
+            $req->header('x-content-sha256' => $value);
+        }
+        else {
+            $value = $req->header($part);
+        }
+        push @$signing_parts, "$part: $value";
+    }
+    my $signing_string = join('\n', @$signing_parts);
+
+    return $signing_string;
+}
+
+=head2 _compute_signature
+
+=cut
+sub _compute_signature {
+    my $self = shift;
+    my $req  = shift;
+
+    my $signing_string = $self->_get_signing_string($req);
+
+    # This method doesn't work, but I don't know why.
+    #
+    # my $filename = '/etc/oess/oracle_rsa';
+    # open my $fh, '<', $filename or die "error opening $filename: $!";
+    # my $key = do { local $/; <$fh> };
+    
+    # my $rsa = Crypt::OpenSSL::RSA->new_private_key($key);
+    # $rsa->use_sha256_hash;
+    # my $signature = $rsa->sign($signing_string);
+    # return encode_base64($signature);
+
+    return `printf '%b' "$signing_string" | openssl dgst -sha256 -sign /etc/oess/oracle_rsa | openssl enc -e -base64 | tr -d '\n'`;
 }
 
 =head2 get_virtual_circuit
 
-=cut
-sub get_virtual_circuit {
-    my $self = shift;
+    my ($virtual_circuit, $err) = $oracle->get_virtual_circuit($virtual_circuit_id);
+    die $err if defined $err;
 
-    return {
+Returns
+
+    {
         "id" =>  "UniqueVirtualCircuitId123",
         "displayName" => "ProviderTestVirtualCircuit",
         "providerName" => "AlexanderGrahamBell",
@@ -57,7 +190,23 @@ sub get_virtual_circuit {
         "providerState" => "INACTIVE",
         "bgpSessionState" => "DOWN",
         "region" => "IAD"
-    };
+    }
+
+=cut
+sub get_virtual_circuit {
+    my $self = shift;
+    my $virtual_circuit_id = shift;
+
+    my $req = new HTTP::Request(GET => "https://iaas.us-ashburn-1.oraclecloud.com/20160918/virtualCircuits/$virtual_circuit_id");
+    my $res = $self->{conn}->request($req);
+    if ($res->code < 200 || $res->code > 299) {
+        my $code  = $res->code;
+        my $error = decode_json($res->content);
+        return (undef, "[$code] $error->{code}: $error->{message}");
+    }
+
+    my $results = decode_json($res->content);
+    return ($results, undef);
 }
 
 =head2 get_virtual_circuits
@@ -65,11 +214,9 @@ sub get_virtual_circuit {
     my ($virtual_circuits, $err) = $oracle->get_virtual_circuits();
     die $err if defined $err;
 
-=cut
-sub get_virtual_circuits {
-    my $self = shift;
+Returns:
 
-    my $results = [
+    [
         {
             "id" =>  "UniqueVirtualCircuitId123",
             "displayName" => "ProviderTestVirtualCircuit",
@@ -90,11 +237,64 @@ sub get_virtual_circuits {
             "bgpSessionState" => "DOWN",
             "region" => "IAD"
         }
-    ];
+    ]
+
+=cut
+sub get_virtual_circuits {
+    my $self = shift;
+
+    my $req = new HTTP::Request(GET => "https://iaas.us-ashburn-1.oraclecloud.com/20160918/virtualCircuits?compartmentId=ocid1.compartment.oc1..aaaaaaaamw3ulplxrs5rb6uwzx33grbvf7kitprgyclrfejpepm43k2zuiaa");
+    my $res = $self->{conn}->request($req);
+    if ($res->code < 200 || $res->code > 299) {
+        my $code  = $res->code;
+        my $error = decode_json($res->content);
+        return (undef, "[$code] $error->{code}: $error->{message}");
+    }
+
+    my $results = decode_json($res->content);
     return ($results, undef);
 }
 
 =head2 update_virtual_circuit
+
+    my ($virtual_circuit, $err) = $oracle->get_virtual_circuit(
+        ocid      => undef, # Oracle Cloud Identifier
+        name      => '',    # User-friendly name. Not unique. Changeable. No confidential info.
+        type      => 'l2',  # l2 or l3
+        bandwidth => 50,    # Converted from int to something like 2Gbps
+        auth_key  => '',    # Empty or Null disables BGP Auth
+        mtu       => 1500,  # Coverted from int to MTU_1500 or MTU_9000
+        oess_asn  => undef,
+        oess_ip   => undef, # /31
+        peer_ip   => undef, # /31
+        oess_ipv6 => undef, # /127
+        peer_ipv6 => undef, # /127
+        vlan      => undef,
+    );
+    die $err if defined $err;
+
+Returns:
+
+    {
+        "id" =>  "UniqueVirtualCircuitId123",
+        "displayName" => "ProviderTestVirtualCircuit",
+        "providerName" => "AlexanderGrahamBell",
+        "providerServiceName" => "Layer2 Service",
+        "providerServiceId" => "ocid1.providerservice.oc1.eu-frankfurt-1.abcd",
+        "serviceType" => "LAYER2",
+        "bgpManagement" => "CUSTOMER_MANAGED",
+        "bandwidthShapeName" => "5Gbps",
+        "oracleBgpAsn" => "561",
+        "customerBgpAsn" => "1234",
+        "crossConnectMappings" => [{
+            "oracleBgpPeeringIp" => "10.0.0.19/31",
+            "customerBgpPeeringIp" => "10.0.0.18/31",
+        }],
+        "lifecycleState" => "PENDING_PROVIDER",
+        "providerState" => "INACTIVE",
+        "bgpSessionState" => "DOWN",
+        "region" => "IAD"
+    }
 
 =cut
 sub update_virtual_circuit {
@@ -153,36 +353,30 @@ sub update_virtual_circuit {
         ipMtu => $mtu
     };
 
-    return {
-        "id" =>  "UniqueVirtualCircuitId123",
-        "displayName" => "ProviderTestVirtualCircuit",
-        "providerName" => "AlexanderGrahamBell",
-        "providerServiceName" => "Layer2 Service",
-        "providerServiceId" => "ocid1.providerservice.oc1.eu-frankfurt-1.abcd",
-        "serviceType" => "LAYER2",
-        "bgpManagement" => "CUSTOMER_MANAGED",
-        "bandwidthShapeName" => "5Gbps",
-        "oracleBgpAsn" => "561",
-        "customerBgpAsn" => "1234",
-        "crossConnectMappings" => [{
-            "oracleBgpPeeringIp" => "10.0.0.19/31",
-            "customerBgpPeeringIp" => "10.0.0.18/31",
-        }],
-        "lifecycleState" => "PENDING_PROVIDER",
-        "providerState" => "INACTIVE",
-        "bgpSessionState" => "DOWN",
-        "region" => "IAD"
-    };
+
+    my $req = new HTTP::Request(PUT => "https://iaas.us-ashburn-1.oraclecloud.com/20160918/virtualCircuits/$args->{ocid}");
+    my $payload = ($args->{type} eq 'l2') ? $l2_payload : $l3_payload;
+    $req->content(encode_json($payload));
+
+    my $res = $self->{conn}->request($req);
+    if ($res->code < 200 || $res->code > 299) {
+        my $code  = $res->code;
+        my $error = decode_json($res->content);
+        return (undef, "[$code] $error->{code}: $error->{message}");
+    }
+    my $results = decode_json($res->content);
+
+    return ($results, undef);
 }
 
 =head2 delete_virtual_circuit
 
-=cut
-sub delete_virtual_circuit {
-    my $self = shift;
-    my $id = shift;
+    my ($virtual_circuit, $err) = $oracle->get_virtual_circuit($virtual_circuit_id);
+    die $err if defined $err;
 
-    return {
+Returns:
+
+    {
         "id" =>  "UniqueVirtualCircuitId456",
         "displayName" => "ProviderTestVirtualCircuit",
         "providerName" => "AlexanderGrahamBell",
@@ -204,7 +398,31 @@ sub delete_virtual_circuit {
         "bgpSessionState" => "UP",
         "region" => "IAD",
         "type" => "PRIVATE"
-    };
+    }
+
+=cut
+sub delete_virtual_circuit {
+    my $self = shift;
+    my $virtual_circuit_id = shift;
+
+    my $req = new HTTP::Request(GET => "https://iaas.us-ashburn-1.oraclecloud.com/20160918/virtualCircuits/$virtual_circuit_id");
+    my $res = $self->{conn}->request($req);
+    if ($res->code < 200 || $res->code > 299) {
+        my $code  = $res->code;
+        my $error = decode_json($res->content);
+        return (undef, "[$code] $error->{code}: $error->{message}");
+    }
+    my $results = decode_json($res->content);
+
+    $req = new HTTP::Request(DELETE => "https://iaas.us-ashburn-1.oraclecloud.com/20160918/virtualCircuits/$virtual_circuit_id");
+    $res = $self->{conn}->request($req);
+    if ($res->code < 200 || $res->code > 299) {
+        my $code  = $res->code;
+        my $error = decode_json($res->content);
+        return (undef, "[$code] $error->{code}: $error->{message}");
+    }
+
+    return ($results, undef);
 }
 
 return 1;
